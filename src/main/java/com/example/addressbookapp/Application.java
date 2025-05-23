191be8a3770a24d75a7f63ac4c5f5a34cc258813
Sukesh03 <sukesh.pamujula@gmail.com>
2025-05-23 17:37:10 +0530

    uc8-Determine the Logging Levels, Logging to Console or File, Logging Patterns, etc based on this application running in Dev, Staging or Production

--- src/main/java/com/example/addressbookapp/Application.java
+++ src/main/java/com/example/addressbookapp/Application.java
@@ -2,12 +2,22 @@ package com.example.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @Autowired
+    private Environment env;
 
+    public static void main(String[] args) {
+        var context = SpringApplication.run(Application.class, args);
+
+        // Access Environment from context to log active profiles
+        Environment env = context.getEnvironment();
+        log.info("Active Spring profiles: {}", (Object) env.getActiveProfiles());
+    }
 }
