e721029140f13c5a01f2b6d7a2af30dac9376ebc
Sukesh03 <sukesh.pamujula@gmail.com>
2025-05-23 16:21:01 +0530

    uc6-Use Lombok Library to auto generate getters and setters for the DTO

--- src/main/java/com/example/addressbookapp/model/AddressBookData.java
+++ src/main/java/com/example/addressbookapp/model/AddressBookData.java
@@ -1,63 +1,25 @@
 package com.example.addressbookapp.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressBookData {
-
+    
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
+    
     private String name;
     private String address;
     private String phone;
-
-    public AddressBookData() {
-    }
-
-    public AddressBookData(String name, String address, String phone) {
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
-    }
-
-    public AddressBookData(int id, String name, String address, String phone) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
 }
